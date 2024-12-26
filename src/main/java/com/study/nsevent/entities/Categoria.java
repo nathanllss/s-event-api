5d8c7441183dd4657b37ab50fe65865e91ac1af8
Nathan <nathan.llucasl@gmail.com>
2024-12-26 12:26:45 -0300

    add: atividade, bloco, categoria and participante entities, h2 configs

---
+++ src/main/java/com/study/nsevent/entities/Categoria.java
@@ -0,0 +1,46 @@
+package com.study.nsevent.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+    @OneToMany(mappedBy = "categoria")
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+}
