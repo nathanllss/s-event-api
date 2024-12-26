9a039e5818f9e9d4a56ffde7a6a976f4ed5ae017
Nathan <nathan.llucasl@gmail.com>
2024-12-26 11:51:56 -0300

    initializing repo

---
+++ src/main/java/com/study/nsevent/NseventApplication.java
@@ -0,0 +1,13 @@
+package com.study.nsevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NseventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NseventApplication.class, args);
+	}
+
+}
